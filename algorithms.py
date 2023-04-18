ae37786307fb547ffc35ee968dd27ec2c91f1852
Win Cheng <wincheng@Wins-MacBook-Pro.local>
2023-04-17 23:13:36 -0700

    separated different algorithms into algorithm.py // Except for Astar which needs more understanding

--- algorithms.py
+++ algorithms.py
@@ -1,14 +1,106 @@
+# algorithms.py
+from queue import PriorityQueue
+from collections import deque
 
-def astar():
-    return
+#heuristic used in A*
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
 
-def BFS():
-    return
+# A* Algorithm
+def astar_algorithm(draw, grid, start, end, reconstruct_path):
+    #needs to understand astar here 
+    return 
+	
+
+# Dijkstra's Algorithm
+def dijkstra_algorithm(draw, grid, start, end, reconstruct_path):
+    dist = {spot: float("inf") for row in grid for spot in row}
+    dist[start] = 0
+    unvisited = {spot for row in grid for spot in row}
+
+    while unvisited:
+        min_spot = min(unvisited, key=lambda spot: dist[spot])
+
+        if dist[min_spot] == float("inf"):
+            break
+
+        if min_spot == end:
+            reconstruct_path(min_spot.came_from, end, draw)
+            end.make_end()
+            return True
+
+        unvisited.remove(min_spot)
+
+        for neighbor in min_spot.neighbors:
+            alt = dist[min_spot] + 1
+            if alt < dist[neighbor]:
+                neighbor.came_from = min_spot
+                dist[neighbor] = alt
+                if neighbor != end:
+                    neighbor.make_open()
+
+        draw()
+
+        if min_spot != start:
+            min_spot.make_closed()
+
+    return False
 
 
-def DFS():
-    return
+# BFS Algorithm
+def bfs_algorithm(draw, grid, start, end, reconstruct_path):
+    queue = deque([start])
+    visited = set([start])
 
+    while queue:
+        current = queue.popleft()
 
-def Djikstra():
-    return
+        if current == end:
+            reconstruct_path(current.came_from, end, draw)
+            end.make_end()
+            return True
+
+        for neighbor in current.neighbors:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                neighbor.came_from = current
+                queue.append(neighbor)
+                if neighbor != end:
+                    neighbor.make_open()
+
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False
+
+# DFS Algorithm
+def dfs_algorithm(draw, grid, start, end):
+    stack = [start]
+    visited = set([start])
+
+    while stack:
+        current = stack.pop()
+
+        if current == end:
+            reconstruct_path(current.came_from, end, draw)
+            end.make_end()
+            return True
+
+        for neighbor in current.neighbors:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                neighbor.came_from = current
+                stack.append(neighbor)
+                if neighbor != end:
+                    neighbor.make_open()
+
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import pygame
 import math
 from queue import PriorityQueue
+import algorithms
 
-screenWidth = 800
-screenWindow = pygame.display.set_mode((screenWidth, screenWidth))
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* Path Finding Algorithm")
 
-colorRed = (255, 0, 0)
-colorGreen = (0, 255, 0)
-colorBlue = (0, 255, 0)
-colorYellow = (255, 255, 0)
-colorWhite = (255, 255, 255)
-colorBlack = (0, 0, 0)
-colorPurple = (128, 0, 128)
-colorOrange = (255, 165 ,0)
-colorGrey = (128, 128, 128)
-
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
 
 class Spot:
 	def __init__(self, row, col, width, total_rows):
@@ -22,7 +24,7 @@ class Spot:
 		self.col = col
 		self.x = row * width
 		self.y = col * width
-		self.color = colorWhite
+		self.color = WHITE
 		self.neighbors = []
 		self.width = width
 		self.total_rows = total_rows
@@ -31,40 +33,40 @@ class Spot:
 		return self.row, self.col
 
 	def is_closed(self):
-		return self.color == colorRed
+		return self.color == RED
 
 	def is_open(self):
-		return self.color == colorGreen
+		return self.color == GREEN
 
 	def is_barrier(self):
-		return self.color == colorBlack
+		return self.color == BLACK
 
 	def is_start(self):
-		return self.color == colorOrange
+		return self.color == ORANGE
 
 	def is_end(self):
-		return self.color == colorBlue
+		return self.color == TURQUOISE
 
 	def reset(self):
-		self.color = colorWhite
+		self.color = WHITE
 
 	def make_start(self):
-		self.color = colorOrange
+		self.color = ORANGE
 
 	def make_closed(self):
-		self.color = colorRed
+		self.color = RED
 
 	def make_open(self):
-		self.color = colorGreen
+		self.color = GREEN
 
 	def make_barrier(self):
-		self.color = colorBlack
+		self.color = BLACK
 
 	def make_end(self):
-		self.color = colorBlue
+		self.color = TURQUOISE
 
 	def make_path(self):
-		self.color = colorPurple
+		self.color = PURPLE
 
 	def draw(self, win):
 		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
@@ -82,9 +84,102 @@ class Spot:
 
 		if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # LEFT
 			self.neighbors.append(grid[self.row][self.col - 1])
-		
 
-			
+	def __lt__(self, other):
+		return False
+
+
+def h(p1, p2):
+	x1, y1 = p1
+	x2, y2 = p2
+	return abs(x1 - x2) + abs(y1 - y2)
+
+
+def reconstruct_path(came_from, current, draw):
+	while current in came_from:
+		current = came_from[current]
+		current.make_path()
+		draw()
+
+def dijkstra_algorithm(draw, grid, start, end):
+    dist = {spot: float("inf") for row in grid for spot in row}
+    dist[start] = 0
+    unvisited = {spot for row in grid for spot in row}
+
+    while unvisited:
+        min_spot = min(unvisited, key=lambda spot: dist[spot])
+
+        if dist[min_spot] == float("inf"):
+            break
+
+        if min_spot == end:
+            reconstruct_path(min_spot.came_from, end, draw)
+            end.make_end()
+            return True
+
+        unvisited.remove(min_spot)
+
+        for neighbor in min_spot.neighbors:
+            alt = dist[min_spot] + 1
+            if alt < dist[neighbor]:
+                neighbor.came_from = min_spot
+                dist[neighbor] = alt
+                if neighbor != end:
+                    neighbor.make_open()
+
+        draw()
+
+        if min_spot != start:
+            min_spot.make_closed()
+
+    return False
+
+def algorithm(draw, grid, start, end):
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0, count, start))
+	came_from = {}
+	g_score = {spot: float("inf") for row in grid for spot in row}
+	g_score[start] = 0
+	f_score = {spot: float("inf") for row in grid for spot in row}
+	f_score[start] = h(start.get_pos(), end.get_pos())
+
+	open_set_hash = {start}
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+		current = open_set.get()[2]
+		open_set_hash.remove(current)
+
+		if current == end:
+			reconstruct_path(came_from, end, draw)
+			end.make_end()
+			return True
+
+		for neighbor in current.neighbors:
+			temp_g_score = g_score[current] + 1
+
+			if temp_g_score < g_score[neighbor]:
+				came_from[neighbor] = current
+				g_score[neighbor] = temp_g_score
+				f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+				if neighbor not in open_set_hash:
+					count += 1
+					open_set.put((f_score[neighbor], count, neighbor))
+					open_set_hash.add(neighbor)
+					neighbor.make_open()
+
+		draw()
+
+		if current != start:
+			current.make_closed()
+
+	return False
+
+
 def make_grid(rows, width):
 	grid = []
 	gap = width // rows
@@ -96,16 +191,17 @@ def make_grid(rows, width):
 
 	return grid
 
+
 def draw_grid(win, rows, width):
 	gap = width // rows
 	for i in range(rows):
-		pygame.draw.line(win, colorGrey, (0, i * gap), (width, i * gap))
+		pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
 		for j in range(rows):
-			pygame.draw.line(win, colorGrey, (j * gap, 0), (j * gap, width))
+			pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
 
 
 def draw(win, grid, rows, width):
-	win.fill(colorWhite)
+	win.fill(WHITE)
 
 	for row in grid:
 		for spot in row:
@@ -113,28 +209,71 @@ def draw(win, grid, rows, width):
 
 	draw_grid(win, rows, width)
 	pygame.display.update()
-	
-def heuristic(a, b):
-	x1, y1 = a
-	x2, y2 = b
-	
-	d = abs(x1 - x2) + abs(y1 - y2)
-	return d
 
 
-	
-def main():
-	totalRows = 50
-	openSet = []
-	closedSet = []
+def get_clicked_pos(pos, rows, width):
+	gap = width // rows
+	y, x = pos
+
+	row = y // gap
+	col = x // gap
+
+	return row, col
+
+
+def main(win, width):
+	ROWS = 50
+	grid = make_grid(ROWS, width)
 
 	start = None
-	end = None 
-	return 
-	
-	
+	end = None
+
+	run = True
+	while run:
+		draw(win, grid, ROWS, width)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+			if pygame.mouse.get_pressed()[0]: # LEFT
+				pos = pygame.mouse.get_pos()
+				row, col = get_clicked_pos(pos, ROWS, width)
+				spot = grid[row][col]
+				if not start and spot != end:
+					start = spot
+					start.make_start()
+
+				elif not end and spot != start:
+					end = spot
+					end.make_end()
+
+				elif spot != end and spot != start:
+					spot.make_barrier()
+
+			elif pygame.mouse.get_pressed()[2]: # RIGHT
+				pos = pygame.mouse.get_pos()
+				row, col = get_clicked_pos(pos, ROWS, width)
+				spot = grid[row][col]
+				spot.reset()
+				if spot == start:
+					start = None
+				elif spot == end:
+					end = None
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE and start and end:
+					for row in grid:
+						for spot in row:
+							spot.update_neighbors(grid)
+
+					algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
 
 
-	
+				if event.key == pygame.K_c:
+					start = None
+					end = None
+					grid = make_grid(ROWS, width)
 
+	pygame.quit()
 
+main(WIN, WIDTH)
